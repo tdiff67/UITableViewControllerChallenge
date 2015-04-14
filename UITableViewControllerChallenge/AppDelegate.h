28b7c7efe5901a45bc42d5d2385d031578c9da57
Thomas Diffendal <thomasdiffendal@Thomass-MacBook-Pro-4.local>
2015-04-13 23:06:56 -0400

    Initial Commit

---
+++ UITableViewControllerChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewControllerChallenge
+//
+//  Created by Thomas Diffendal on 4/13/15.
+//  Copyright (c) 2015 ApaTapA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewControllerChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewControllerChallenge
+//
+//  Created by Thomas Diffendal on 4/13/15.
+//  Copyright (c) 2015 ApaTapA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
